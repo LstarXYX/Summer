7bd7b433d605fab6e7bad9908509534e39b97d19
LstarXYX <294554846@qq.com>
2019-07-14 13:54:32 +0800

    test2

---
+++ script/ShowPic.js
@@ -0,0 +1,13 @@
+function ShowPic(whichpic){
+    var source = whichpic.getAttribute("href");
+    var text = whichpic.getAttribute("title");
+    var placeholder = document.getElementById("placeholder");
+    var description = document.getElementById("description");
+    placeholder.setAttribute("src",source);
+    description.firstChild.nodeValue = text;
+    
+}
+function countBodyChildren(){
+    var body_element = document.getElementsByTagName("body")[0];
+    alert(body_element.childNodes.length);
+}
